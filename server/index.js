1d64c74878e2b55cbaf3d299d3f527d92edf3d38
Jimmy Hung <hungmeenhong@gmail.com>
2019-05-24 23:03:26 -0500

    3rd commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.static('./public'))
 
 
 app.get('/api/images', (req, res)=>{
-    findByUuid(1000, (err, data)=>{
+    findByUuid(1001, (err, data)=>{
       if(err){console.log('cannot query find')
       } else {
           res.send(data);
